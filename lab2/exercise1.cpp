8e10fd3d6e7d6504f01df4a11aee8013bbf08b40
Finian Mcdara Bradwell <FINIAN.BRADWELL.2017@mumail.ie>
2018-10-09 17:31:21 +0100

    Added user input functionality

--- lab2/exercise1.cpp
+++ lab2/exercise1.cpp
@@ -6,3 +6,11 @@
 #include <vector>
 using std::cin; using std::cout; using std::end1; using std::setprecision;
 using std::vector; using std::sort; using std::streamsize; using std::string;
+int main() {
+    cout << "PLease enter your midterm and final exam grades: ";
+    double midterm, final;
+    cin >> midterm >> final;
+    cout << "Enter all your homework followed by end-of-file: ";
+    vector<double> homework;
+    double x;
+}
