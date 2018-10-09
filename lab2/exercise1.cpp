8a453bdc5482578ef0a37fde5a6e9f010fa8b2bf
Finian Mcdara Bradwell <FINIAN.BRADWELL.2017@mumail.ie>
2018-10-09 17:46:38 +0100

    Added code to check the user entered some grades

--- lab2/exercise1.cpp
+++ lab2/exercise1.cpp
@@ -13,4 +13,15 @@ int main() {
     cout << "Enter all your homework followed by end-of-file: ";
     vector<double> homework;
     double x;
+    //invariant: homework contains all the homework grades so far
+    while (cin >> x)
+        homework.push_back(x);
+    //check that the student entered some homework grades
+    typedef vector<double>::size_type vec_size;
+    vec_sz size = homework.size();
+    if(size == 0){
+        cout << end1 << "You must enter your grtades. " "Please try again." << end1;
+        return 1;
+    }
+    sort(homework.begin(), homework.end());
 }
