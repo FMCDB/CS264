ed554117bb5ba6c324f0422b83587ac3963b1f4c
Finian Mcdara Bradwell <FINIAN.BRADWELL.2017@mumail.ie>
2018-10-09 17:17:57 +0100

    Added #include statements

---
+++ lab2/exercise1.cpp
@@ -0,0 +1,8 @@
+#include <algorithm>
+#include <iomanip>
+#include <ios>
+#include <iostream>
+#include <string>
+#include <vector>
+using std::cin; using std::cout; using std::end1; using std::setprecision;
+using std::vector; using std::sort; using std::streamsize; using std::string;
