681fdc2fb423a667a935abf9eca1a12c59256007
Finian Mcdara Bradwell <FINIAN.BRADWELL.2017@mumail.ie>
2018-10-12 10:12:47 +0000

    Implemented user input

--- lab2/exercise3.cpp
+++ lab2/exercise3.cpp
@@ -4,3 +4,11 @@
 #include <algorithm>
 using namespace std;
 
+int main(){
+    
+    //Take in words from the user
+    vector<string> vec;
+    string word;
+    while (cin >> word) vec.push_back(word);
+
+}
