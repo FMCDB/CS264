cf46431ddf0ed7596411524e861986334456d976
Finian Mcdara Bradwell <FINIAN.BRADWELL.2017@mumail.ie>
2018-10-12 10:20:41 +0000

    Implemented for loop to find the longest and shortest entries

--- lab2/exercise3.cpp
+++ lab2/exercise3.cpp
@@ -5,10 +5,16 @@
 using namespace std;
 
 int main(){
-    
+
     //Take in words from the user
     vector<string> vec;
     string word;
     while (cin >> word) vec.push_back(word);
 
+    //Loop through the vector to find the longest and shortest entries
+    string max = vec[0], min = vec[0];
+    for(int i = 0; i < vec.size(); i++){
+        if(vec[i].size() > max.size()) max = vec[i];
+        else if(vec[i].size() < min.size()) min  = vec[i];
+    }
 }
