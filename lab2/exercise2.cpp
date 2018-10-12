6b7bbd6ea16a1126e2aa7abf4c038397e14f3994
Finian Mcdara Bradwell <FINIAN.BRADWELL.2017@mumail.ie>
2018-10-12 09:54:48 +0000

    Added for loop to count number of occurrences each word appears in input

--- lab2/exercise2.cpp
+++ lab2/exercise2.cpp
@@ -14,7 +14,18 @@ int main(){
     //Sort string vector
     sort(vec.begin(), vec.end());
 
+    //Count the number of times each word appears in the input
+    word = vec[0];
+    int count = 1;
+    for(int i = 1; i < vec.size(); i++){
+        if(vec[i] != word){
+            cout << word << " appears " << count << " times" << endl;
+            word = vec[i];
+            count = 0;
+        }
+        count++;
+    }
+    cout << word << " appears " << count << " times" << endl;
 
-    
-
+    return 0;
 }
