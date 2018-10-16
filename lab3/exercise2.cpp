66085c72f8f44d9c6b0899c415bff443c780be70
Finian Mcdara Bradwell <FINIAN.BRADWELL.2017@mumail.ie>
2018-10-16 16:42:50 +0100

    Implemented user input

---
+++ lab3/exercise2.cpp
@@ -0,0 +1,15 @@
+#include <iomanip>
+#include <ios>
+#include <iostream>
+#include <string>
+using std::cin; using std::cout; using std::endl;
+using std::setprecision; using std::string; using std::streamsize;
+int main(){
+    int pos = 0, neg = 0;
+    cout << "Enter a positive number: ";
+    cin >> pos;
+    cout << "Enter a negative number: ";
+    cin >> neg;
+
+    return 0;
+}
