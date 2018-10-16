afe84716056b9e5e8579b04a422b95063c1b3e0e
Finian Mcdara Bradwell <FINIAN.BRADWELL.2017@mumail.ie>
2018-10-16 16:46:14 +0100

    Implemented for loop to print range of numbers entered by user

--- lab3/exercise2.cpp
+++ lab3/exercise2.cpp
@@ -1,3 +1,4 @@
+//Finian BRADWELL 16306561
 #include <iomanip>
 #include <ios>
 #include <iostream>
@@ -5,11 +6,18 @@
 using std::cin; using std::cout; using std::endl;
 using std::setprecision; using std::string; using std::streamsize;
 int main(){
+
+    //Get positive and negative numbers from the user
     int pos = 0, neg = 0;
     cout << "Enter a positive number: ";
     cin >> pos;
     cout << "Enter a negative number: ";
     cin >> neg;
+
+    //Print the range of the entered numbers line by line from positive to negative
+    for(int i = pos; i > neg-1; i--){
+        cout << i << endl;
+    }
 
     return 0;
 }
