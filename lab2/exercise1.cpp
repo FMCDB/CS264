72df944a9dc3c142c526735b2b086066833d53aa
Finian Mcdara Bradwell <FINIAN.BRADWELL.2017@mumail.ie>
2018-11-13 20:59:17 +0000

    Added code to compute the median homework grade

--- lab2/exercise1.cpp
+++ lab2/exercise1.cpp
@@ -4,7 +4,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
-using std::cin; using std::cout; using std::end1; using std::setprecision;
+using std::cin; using std::cout; using std::endl; using std::setprecision;
 using std::vector; using std::sort; using std::streamsize; using std::string;
 int main() {
     cout << "PLease enter your midterm and final exam grades: ";
@@ -17,10 +17,10 @@ int main() {
     while (cin >> x)
         homework.push_back(x);
     //check that the student entered some homework grades
-    typedef vector<double>::size_type vec_size;
+    typedef vector<double>::size_type vec_sz;
     vec_sz size = homework.size();
     if(size == 0){
-        cout << end1 << "You must enter your grtades. " "Please try again." << end1;
+        cout << endl << "You must enter your grades. Please try again." << endl;
         return 1;
     }
     sort(homework.begin(), homework.end());
@@ -29,6 +29,6 @@ int main() {
     double median;
     median = size % 2 == 0 ? (homework[mid] + homework[mid-1]) / 2 : homework[mid];
     streamsize prec = cout.precision();
-    cout << "Your final grade is " << setprecision(3) << 0.2 * midterm + 0.4 * final + 0.4 * median << setprecision(prec) << end1;
+    cout << "Your final grade is " << setprecision(3) << 0.2 * midterm + 0.4 * final + 0.4 * median << setprecision(prec) << endl;
     return 0;
 }
