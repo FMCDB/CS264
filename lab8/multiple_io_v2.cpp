65bd7481c2ddf789971405ba14fb75b2309e80e0
Finian Mcdara Bradwell <FINIAN.BRADWELL.2017@mumail.ie>
2018-12-03 19:13:29 +0000

    Compiled and ran file using using two files containging random text.

---
+++ lab8/multiple_io_v2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using std::ifstream; using std::string; using std::cout; using std::endl;
+using std::cerr;
+int main(int argc, char **argv){
+    int fail_count = 0;
+    // for each file in input list
+    for(int i = 1; i < argc; ++i){
+        ifstream in(argv[i]);
+        // if it exists, write its contents, otherwise generate an error message
+        if(in){
+            string s;
+            while(getline(in,s)){
+                cout << s << endl;
+            }
+        }
+        else{
+            cerr << "cannot open file " << argv[i] << endl;
+            ++fail_count;
+        }
+    }
+    return fail_count;
+}
